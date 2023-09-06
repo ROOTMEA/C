6db2d557fe9ddd124dde41626de2b33f84f33488
Efe Aygün <82315578+ROOTMEA@users.noreply.github.com>
2023-09-06 08:17:54 +0300

    Add files via upload

---
+++ c/project/file_example.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *ftpr;
+
+    //Create a file and file name
+    char file_name[10];
+
+    printf("Enter the file name:");
+    scanf("%s",file_name);
+
+    ftpr=fopen(file_name,"a");
+    fclose(ftpr);
+
+    //Appened the text in file
+    fopen(file_name,"a");
+    char text[100];
+    printf("Enter the text:");
+    scanf("%s",text);
+
+    fprintf(ftpr,"%s\n",text);
+    fclose(ftpr);
+
+    //Read the file
+    fopen(file_name,"r");
+    char myString[100];
+    fgets(myString,100,ftpr);
+    printf("%s",myString);
+    fclose(ftpr);   
+
+    return 0;
+}
